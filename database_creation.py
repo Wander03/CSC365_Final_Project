1c6b673d0df6eeb653f9221d1730f9c7f40d24ef
Wander03 <adkerr2001@gmail.com>
2022-11-07 23:58:59 -0800

    table creation

---
+++ database_creation.py
@@ -0,0 +1,161 @@
+import os
+import urllib.parse
+import sqlalchemy as sa
+from dotenv import load_dotenv
+from sqlalchemy import Table, Column, Integer, String, Date, DateTime, DECIMAL, Boolean, ForeignKey
+
+
+def main():
+    # Load env file
+    load_dotenv()
+
+    # Connection Parameters (you will sub in with your own databases values)
+    escapedPassword = urllib.parse.quote_plus(os.environ.get("DB_PASSWORD"))
+    sqldialect = os.environ.get("DB_DIALECT")
+    username = os.environ.get("DB_USER")
+    database = os.environ.get("DB_NAME")
+    host = os.environ.get("DB_HOST")
+
+    # Build the connection string based on database specific parameters
+    connectionString = f"{sqldialect}://{username}:{escapedPassword}@{host}/{database}"
+
+    # Create a new DB engine based on our connection string
+    engine = sa.create_engine(connectionString)
+
+    with engine.begin() as conn:
+        meta = sa.MetaData()
+        try:
+            conn.execute(sa.text("SET FOREIGN_KEY_CHECKS = 0"))
+            conn.execute(sa.text("DROP TABLE IF EXISTS betType, walletType, transactionType, map, user, transactions, "
+                                 "wallet, player, teams, pool, matches, bets, results, teamsSnapshot"))
+            conn.execute(sa.text("SET FOREIGN_KEY_CHECKS = 1"))
+
+            Table(
+                'betType', meta,
+                Column('id', Integer, primary_key=True),
+                Column('type', String(50))
+            )
+
+            Table(
+                'walletType', meta,
+                Column('id', Integer, primary_key=True),
+                Column('type', String(50)),
+                Column('canWithdraw', Boolean),
+                Column('canDeposit', Boolean)
+            )
+
+            Table(
+                'transactionType', meta,
+                Column('id', Integer, primary_key=True),
+                Column('type', String(50))
+            )
+
+            Table(
+                'map', meta,
+                Column('id', Integer, primary_key=True),
+                Column('name', String(50))
+            )
+
+            Table(
+                'user', meta,
+                Column('id', Integer, primary_key=True),
+                Column('firstName', String(50)),
+                Column('lastName', String(50)),
+                Column('email', String(50)),
+                Column('username', String(50)),
+                Column('password', String(50)),
+                Column('balance', DECIMAL(50, 2))
+            )
+
+            Table(
+                'transactions', meta,
+                Column('id', Integer, primary_key=True),
+                Column('transactionTypeId', Integer, ForeignKey('transactionType.id')),
+                Column('userId', Integer, ForeignKey('user.id')),
+                Column('amount', DECIMAL(50, 2))
+            )
+
+            Table(
+                'wallet', meta,
+                Column('id', Integer, primary_key=True),
+                Column('userId', Integer, ForeignKey('user.id')),
+                Column('typeId', Integer, ForeignKey('walletType.id')),
+                Column('name', String(50)),
+                Column('amountStored', DECIMAL(50, 2))
+            )
+
+            Table(
+                'player', meta,
+                Column('id', Integer, primary_key=True),
+                Column('name', String(50)),
+                Column('avgRating', DECIMAL(50, 2))
+            )
+
+            Table(
+                'teams', meta,
+                Column('id', Integer, primary_key=True),
+                Column('playerId', Integer, ForeignKey('player.id')),
+                Column('team', String(50))
+            )
+
+            Table(
+                'pool', meta,
+                # Column('id', Integer, primary_key=True),
+                Column('matchId', Integer, ForeignKey('matches.id'), primary_key=True),
+                Column('betType', Integer, ForeignKey('betType.id'), primary_key=True),
+                Column('amount', DECIMAL(50, 2))
+            )
+
+            Table(
+                'matches', meta,
+                Column('id', Integer, primary_key=True),
+                Column('team1Id', Integer, ForeignKey('teams.id')),
+                Column('team2Id', Integer, ForeignKey('teams.id')),
+                Column('mapId', Integer, ForeignKey('map.id')),
+                Column('poolId', Integer),
+                Column('date', Date)
+            )
+
+            Table(
+                'bets', meta,
+                Column('id', Integer, primary_key=True),
+                Column('userId', Integer, ForeignKey('user.id')),
+                Column('matchId', Integer, ForeignKey('matches.id')),
+                Column('amount', DECIMAL(50, 2)),
+                Column('placedTime', DateTime)
+            )
+
+            Table(
+                'results', meta,
+                Column('id', Integer, primary_key=True),
+                Column('matchId', Integer, ForeignKey('matches.id')),
+                Column('totalKills', Integer),
+                Column('headshotCount', Integer),
+                Column('team1Score', Integer),
+                Column('team2Score', Integer)
+            )
+
+            Table(
+                'teamsSnapshot', meta,
+                Column('id', Integer, primary_key=True),
+                Column('playerId', Integer, ForeignKey('player.id')),
+                Column('team', String(50))
+            )
+
+            meta.create_all(engine)
+
+            result = conn.execute(sa.text("show tables"))
+            for i in result:
+                print(i)
+
+        except Exception as error:
+            print(f"Error returned: <<<{error}>>>")
+
+
+if __name__ == '__main__':
+    main()
+
+# while loop for n days
+#     for each agent, give match info, place bet
+#
+#     each agent is an object with a different "goal", eg greedy
